c708d8f852ffa644f0785486a599bc4fe6c8b072
sieghard <sieghard@c17d62a3-0337-0410-9172-b2f70200edfa>
2007-08-06 19:40:09 +0000

    Importing existing sources.

---
+++ trunk/ASDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  iSFV
+//
+//  Created by Albert Sodyl on 01/08/07.
+//  Copyright __MyCompanyName__ 2007 . All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface ASDocument : NSDocument
+{
+}
+@end
