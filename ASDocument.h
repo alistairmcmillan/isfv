02347b460e5d76a88ddb46155555cef31b321c3b
sieghard <sieghard@c17d62a3-0337-0410-9172-b2f70200edfa>
2007-08-07 19:33:25 +0000

    Add GPLv3 license, add preamble to file headers.

--- ASDocument.h
+++ ASDocument.h
@@ -1,11 +1,25 @@
 //
-//  MyDocument.h
+//  ASDocument.h
 //  iSFV
 //
 //  Created by Albert Sodyl on 01/08/07.
-//  Copyright __MyCompanyName__ 2007 . All rights reserved.
+//  Copyright Eoros 2007 . All rights reserved.
 //
 
+//	This file is part of iSFV.
+//
+//	iSFV is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation; either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	iSFV is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #import <Cocoa/Cocoa.h>
 
